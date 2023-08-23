3b3b23e90b7c2a08cf28f84661ac2d635a2bc4a0
Gowtham1802 <gowthama7575@gmail.com>
2023-08-23 14:08:29 +0530

    add coding

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const phoneNumber = document.getElementById('phone-num');
 const dateOfBirth = document.getElementById('id-dob');
 const newPassword = document.getElementById('id-new-password');
 const conPassword = document.getElementById('id-con-password');
-const urlContainer = document.getElementById('id-url');
+const urlLink = document.getElementById('id-url');
 const genderSelect = document.querySelectorAll('input[type=radio]');
 console.log(genderSelect);
 const languageSelect = document.querySelectorAll('input[type=checkbox]');
-const districtSelect = document.querySelector('id-district');
+const districtSelect = document.querySelector('#id-district');
 const rangeSelect = document.getElementById('id-ranges');
 
 const submitBtn = document.getElementById('id-submit');
@@ -19,11 +19,72 @@ const resetBtn = document.getElementById('id-reset');
 
 
 // Global Variables
+let isValidated = false
 
 //  Function
 
 function init(){
-  
+
+}
+
+function checkValidation(){
+  if(!(isValidated === true)){  
+    if(firstName.value === ""){
+      firstName.style.borderColor = "red";
+    }else{
+      firstName.style.borderColor = "green";
+    }
+    if(lastName.value === ""){
+      lastName.style.borderColor = "red";
+    }else{
+      lastName.style.borderColor = "green";
+    }
+    if(eMail.value === ""){
+      eMail.style.borderColor = "red";
+    }else{
+      eMail.style.borderColor = "green";
+    }
+    if(phoneNumber.value === ""){
+      phoneNumber.style.borderColor = "red";
+    }else{
+      phoneNumber.style.borderColor = "green";
+    }
+    if(dateOfBirth.value === ""){
+      dateOfBirth.style.borderColor = "red";
+    }else{
+      dateOfBirth.style.borderColor = "green";
+    }
+    if(newPassword.value === ""){
+      newPassword.style.borderColor = "red"
+    }else{
+      newPassword.style.borderColor = "green";
+    }
+    if(conPassword.value === ""){
+      conPassword.style.borderColor = "red";
+    }else{
+      conPassword.style.borderColor = "green";
+    }
+    if(urlLink.value === ""){
+      urlLink.style.borderColor = "red";
+    }else{
+      urlLink.style.borderColor = "green";
+    }
+    if(genderSelect.value === ""){
+      genderSelect.style.borderColor = "red";
+    }else{
+      genderSelect.style.borderColor = "green";
+    }
+    if(languageSelect.value === ""){
+      languageSelect.style.borderColor = "red";
+    }else{
+      languageSelect.style.borderColor = "green";
+    }
+    if(districtSelect.checked){
+      districtSelect.style.borderColor = "red";
+    }else{
+      districtSelect.style.borderColor = "green";
+    }
+  }
 }
 
 //  Event listener
